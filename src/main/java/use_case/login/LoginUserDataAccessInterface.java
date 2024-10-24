5c6ab1c88ce5f4b8b1047c86fd96e70613edee0e
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-10-23 22:25:12 -0400

    add: task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name the username being set
      */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return the current user
+     */
+    String getCurrentUser();
 }
